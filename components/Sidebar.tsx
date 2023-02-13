577bd191cee90d9ef69c31853f08e090ece703da
Munim_Ali <munimali52@gmail.com>
2023-02-13 21:36:13 +0500

    User Authentication

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
 import NewChat from "./NewChat";
 
 function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -11,6 +15,9 @@ function Sidebar() {
           {/* {Map through the ChatRows} */}
         </div>
       </div>
+      {session && (
+        <img onClick={()=>{signOut()}} src={session.user?.image} alt="Profile Picture" className="w-12 h-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50" />
+      )}
     </div>
   );
 }
